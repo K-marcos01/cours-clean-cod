7996fcdea2353cd5b0e4fa0c2d188accd4e26533
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 14:11:52 +0100

    red: E2 - demi-heure entamee facturee 1.50 euro

--- tp1-Marcos/kata_parking/test_parking.py
+++ tp1-Marcos/kata_parking/test_parking.py
@@ -9,3 +9,11 @@ def test_stationnement_de_30_minutes_est_gratuit():
     montant = calculer_montant(entree, sortie)
 
     assert montant == 0
+
+def test_stationnement_de_45_minutes_facture_une_demi_heure():
+    entree = datetime(2024, 1, 1, 8, 0)
+    sortie = datetime(2024, 1, 1, 8, 45)
+
+    montant = calculer_montant(entree, sortie)
+
+    assert montant == 1.50
