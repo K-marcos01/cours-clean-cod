a3617db16c427a0c5ef31622e376b85c00e1b77f
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 16:41:09 +0100

    green: E3 - plafond applique par tranche de 24h, pas globalement

--- tp1-Marcos/kata_parking/parking.py
+++ tp1-Marcos/kata_parking/parking.py
@@ -1,6 +1,9 @@
 import math
 
+MINUTES_PAR_DEMI_HEURE = 30
+TARIF_DEMI_HEURE = 1.50
 PLAFOND_24H = 18
+MINUTES_PAR_JOUR = 24 * 60
 
 
 def calculer_montant(entree, sortie):
@@ -11,7 +14,10 @@ def calculer_montant(entree, sortie):
         return 0
 
     minutes_facturables = minutes - 30
-    demi_heures = math.ceil(minutes_facturables / 30)
-    montant = demi_heures * 1.50
+    demi_heures = math.ceil(minutes_facturables / MINUTES_PAR_DEMI_HEURE)
+    montant = demi_heures * TARIF_DEMI_HEURE
 
-    return min(montant, PLAFOND_24H)
+    tranches_de_24h = math.ceil(minutes / MINUTES_PAR_JOUR)
+    plafond = PLAFOND_24H * tranches_de_24h
+
+    return min(montant, plafond)
