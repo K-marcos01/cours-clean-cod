b10b147472fa0f324673afe929c2a1a342177740
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 14:16:38 +0100

    green: E2 - demi-heure entamee facturee 1.50 euro

--- tp1-Marcos/kata_parking/parking.py
+++ tp1-Marcos/kata_parking/parking.py
@@ -1,2 +1,13 @@
+import math
+
+
 def calculer_montant(entree, sortie):
-    return 0
+    duree = sortie - entree
+    minutes = duree.total_seconds() / 60
+
+    if minutes <= 30:
+        return 0
+
+    minutes_facturables = minutes - 30
+    demi_heures = math.ceil(minutes_facturables / 30)
+    return demi_heures * 1.50
