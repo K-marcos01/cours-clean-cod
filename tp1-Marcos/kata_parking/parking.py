1e700fd2b3272cc2d71cdec64ef493026701e8ad
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 16:50:26 +0100

    green: E4 - camion abonne paie 60 pourcent

--- tp1-Marcos/kata_parking/parking.py
+++ tp1-Marcos/kata_parking/parking.py
@@ -4,9 +4,10 @@ MINUTES_PAR_DEMI_HEURE = 30
 TARIF_DEMI_HEURE = 1.50
 PLAFOND_24H = 18
 MINUTES_PAR_JOUR = 24 * 60
+REMISE_ABONNE = 0.60
 
 
-def calculer_montant(entree, sortie):
+def calculer_montant(entree, sortie, abonne=False):
     duree = sortie - entree
     minutes = duree.total_seconds() / 60
 
@@ -19,5 +20,9 @@ def calculer_montant(entree, sortie):
 
     tranches_de_24h = math.ceil(minutes / MINUTES_PAR_JOUR)
     plafond = PLAFOND_24H * tranches_de_24h
+    montant = min(montant, plafond)
 
-    return min(montant, plafond)
+    if abonne:
+        montant = montant * REMISE_ABONNE
+
+    return round(montant, 2)
