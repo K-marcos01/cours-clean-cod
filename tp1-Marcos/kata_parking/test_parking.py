d4ec500f65e300dc97b78d757d444c6253790bc7
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 13:47:39 +0100

    red: E1 - stationnement de 30 minutes gratuit

---
+++ tp1-Marcos/kata_parking/test_parking.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from parking import calculer_montant
+
+def test_stationnement_de_30_minutes_est_gratuit():
+    entree = datetime(2024, 1, 1, 8, 0)
+    sortie = datetime(2024, 1, 1, 8, 30)
+
+    montant = calculer_montant(entree, sortie)
+
+    assert montant == 0
