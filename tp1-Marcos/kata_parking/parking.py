8a6ab531acb2f65a66142577b20ab075d1aa6d87
Marcos <kpanoumarcos2@gmail.com>
2026-09-16 23:28:37 +0400

    green: E5 - camion electrique 60 minutes gratuites

--- tp1-Marcos/kata_parking/parking.py
+++ tp1-Marcos/kata_parking/parking.py
@@ -5,16 +5,20 @@ TARIF_DEMI_HEURE = 1.50
 PLAFOND_24H = 18
 MINUTES_PAR_JOUR = 24 * 60
 REMISE_ABONNE = 0.60
+FRANCHISE_NORMALE = 30
+FRANCHISE_ELECTRIQUE = 60
 
 
-def calculer_montant(entree, sortie, abonne=False):
+def calculer_montant(entree, sortie, abonne=False, electrique=False):
     duree = sortie - entree
     minutes = duree.total_seconds() / 60
 
-    if minutes <= 30:
+    franchise = FRANCHISE_ELECTRIQUE if electrique else FRANCHISE_NORMALE
+
+    if minutes <= franchise:
         return 0
 
-    minutes_facturables = minutes - 30
+    minutes_facturables = minutes - franchise
     demi_heures = math.ceil(minutes_facturables / MINUTES_PAR_DEMI_HEURE)
     montant = demi_heures * TARIF_DEMI_HEURE
 
